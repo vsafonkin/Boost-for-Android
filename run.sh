023ee1636b6037aa94c47f9ea7c041c83cc3d1f4
Vladimir Safonkin <vsafonkin@gmail.com>
2020-03-02 16:39:18 +0300

    Brew no quarantine

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+export HOMEBREW_CASK_OPTS="--no-quarantine"
+
 echo "Set env variables..."
 export NDK_ROOT=${ANDROID_SDK_ROOT}/ndk-bundle
 echo "::set-env name=NDK_ROOT::$NDK_ROOT"
@@ -12,8 +14,8 @@ mkdir -p bins/macos
 mkdir -p logs/macos
 
 echo "Install dependencies..."
-brew update
-brew cask install android-ndk
+brew update 
+brew cask install android-ndk --no-c
 export NDK_ROOT="$(brew --prefix)/share/android-ndk"
 echo "::set-env name=NDK_ROOT::$NDK_ROOT"
 
